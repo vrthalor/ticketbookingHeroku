8c90701c3f72b305b8addfea89de095010476e4d
vrthalor <55393604+vrthalor@users.noreply.github.com>
2020-07-27 10:48:12 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/dare2compete',(err)=>{
+// mongoose.connect('mongodb://localhost:27017/dare2compete',(err)=>{
+mongoose.connect('mongodb+srv://vijay:vijay@ticketbooking.vy2hq.mongodb.net/dare2compete?retryWrites=true&w=majority',(err)=>{
+    
     if(!err)
         console.log('mongodb connection succeeded');
     else

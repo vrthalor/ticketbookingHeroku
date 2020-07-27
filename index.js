4da760ad58e1049abe47f4c0a96a4788bd6025da
vrthalor <55393604+vrthalor@users.noreply.github.com>
2020-07-27 10:02:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const express = require('express');
 var cors = require('cors');
 const bodyParser = require('body-parser');
 
-// const { mongoose } = require('./db.js');   commited
-// const router = require('./controllers/route')  commited
+const { mongoose } = require('./db.js'); 
+const router = require('./controllers/route')  
 
 var app = express();
 app.use(cors());
-// app.use('', router);         commited
+app.use('', router);         
 
-app.get('/', function (req, res) {
-  res.send('hello world from heroku server')
-})
+// app.get('/', function (req, res) {
+//   res.send('hello world from heroku server')
+// })
 
 app.use(bodyParser.json());
 var PORT = process.env.PORT || 5000;
